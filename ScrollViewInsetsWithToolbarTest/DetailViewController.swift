0fdfcfd2b5c2658ea4692f14c5d74bbb68764c82
Andrew Ebling <andrew@ebling.me.uk>
2018-07-13 09:04:53 +0100

    Add text view to test keyboard case.

--- ScrollViewInsetsWithToolbarTest/DetailViewController.swift
+++ ScrollViewInsetsWithToolbarTest/DetailViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class DetailViewController: UIViewController {
 
     var scrollView: UIScrollView!
+    var textView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,14 +21,27 @@ class DetailViewController: UIViewController {
         self.view.addSubview(self.scrollView)
         self.scrollView.autoresizingMask = [.flexibleHeight,  .flexibleWidth]
         self.scrollView.contentSize = CGSize(width: 1024, height: 1024)
+        
+        self.textView = UITextView(frame: CGRect(x: 80, y: 80, width: 150, height: 40))
+        self.scrollView.addSubview(self.textView)
+        
         self.navigationController?.isToolbarHidden = false
         
         let bbi = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(hideToolbar(_:)))
-        self.navigationItem.rightBarButtonItem = bbi
+        let editBBI = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editText(_:)))
+        self.navigationItem.rightBarButtonItems = [ bbi, editBBI]
     }
 
-    @IBAction func hideToolbar(_ sender: Any) {
+    @objc func hideToolbar(_ sender: Any) {
         self.navigationController?.isToolbarHidden = !(self.navigationController?.isToolbarHidden ?? true)
+    }
+    
+    @objc func editText(_ sender: Any) {
+        if self.textView.isFirstResponder {
+            self.textView.resignFirstResponder()
+        } else {
+            self.textView.becomeFirstResponder()
+        }
     }
 }
 
